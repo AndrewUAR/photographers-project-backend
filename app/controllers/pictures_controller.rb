fa798d104f0047edcf4797896f4557efc5490b9b
aaron blum <blumaa@gmail.com>
2019-10-22 12:37:23 -0500

    backend now accepts active storage object and saves it to pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,17 @@
 class PicturesController < ApplicationController
+  def index
+    pictures = Picture.all
+    render json: pictures
+  end
+
+  def create
+    picture = Picture.new(name: params[:name], description: params[:description], photographer_id: params[:photographer_id])
+    image = params[:picture]
+    puts image
+    puts "*************************************"
+    picture.image.attach(image)
+    picture.save
+    url = url_for(picture.image)
+    render json: {image: url}
+  end
 end
