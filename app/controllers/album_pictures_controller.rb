66805d4fb0fe6eb7e88d20c8d3899ce0b72a86a1
aaron blum <blumaa@gmail.com>
2019-10-23 16:42:25 -0500

    added create album_pictures controller method

--- app/controllers/album_pictures_controller.rb
+++ app/controllers/album_pictures_controller.rb
@@ -1,2 +1,7 @@
 class AlbumPicturesController < ApplicationController
+  def create
+    puts params[:data][:ablum_id]
+    puts params[:data][:picture_id]
+    album_picture = AlbumPicture.create(album_id: params[:data][:album_id], picture_id: params[:data][:picture_id])
+  end
 end
--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -14,7 +14,7 @@ class AlbumsController < ApplicationController
     album.save
     render json: album
   end
-  
+
   def update
     album = Album.find_by(id: params[:id])
     album.update(name: params[:name], description: params[:description])
@@ -25,5 +25,9 @@ class AlbumsController < ApplicationController
     album = Album.find_by(id: params[:id])
     album.destroy
     render json: album
+  end
+
+  def add_photo_to_album
+
   end
 end
