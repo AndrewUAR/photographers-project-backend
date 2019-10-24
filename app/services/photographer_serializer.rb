943703b9714f1b8a5a0f6d0a09ce718fafe30803
aaron blum <blumaa@gmail.com>
2019-10-24 11:12:29 -0500

    changed photographer serializer and controller to update and accept active storage images

--- app/services/photographer_serializer.rb
+++ app/services/photographer_serializer.rb
@@ -1,13 +1,22 @@
-class PhotographerSerializer
-  def initialize(photographer_object)
-    @photographer = photographer_object
-  end
+class PhotographerSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
 
-  def to_serialized_json
-    @photographer.to_json(:include => {
-      :pictures => {:only => [:id, :name, :description]},
-      :albums => {:only => [:id, :name, :description]}
-    }, :except => [:updated_at, :created_at])
+  attributes :image, :id, :name, :birthdate, :bio, :start_date, :city
+
+  def image
+      rails_blob_path(object.image, only_path: true) if object.image.attached?
   end
+  #
+  #
+  # def initialize(photographer_object)
+  #   @photographer = photographer_object
+  # end
+  #
+  # def to_serialized_json
+  #   @photographer.to_json(:include => {
+  #     :pictures => {:only => [:id, :name, :description]},
+  #     :albums => {:only => [:id, :name, :description]}
+  #   }, :except => [:updated_at, :created_at])
+  # end
 
 end
