5b203ece1c845bdd1f04c7ba0623eab82882f948
Andrew <andrewriznykua93@gmail.com>
2019-10-25 09:57:49 -0500

    Likes

--- app/services/picture_serializer.rb
+++ app/services/picture_serializer.rb
@@ -1,7 +1,7 @@
 class PictureSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
 
-    attributes :image, :id, :name, :description, :photographer_id, :albums
+    attributes :image, :id, :name, :description, :photographer_id, :albums, :likes
 
     def image
         rails_blob_path(object.image, only_path: true) if object.image.attached?
